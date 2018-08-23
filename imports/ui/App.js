17bc65febc40423ec32c6ff067d0374de8b726c9
Cristhian Arostegui <cr.arostegui@gmail.com>
2018-08-23 15:56:49 -0400

    added hide completed task feature, and pending task counter.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,15 @@ import Task from './Task.js';
  
 // App component - represents the whole app
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
  
@@ -30,10 +39,21 @@ class App extends Component {
       { _id: 3, text: 'This is task 3' },
     ];
   }
- 
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
     //return this.getTasks().map((task) => (
-    return this.props.tasks.map((task) => (
+    //return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -42,8 +62,19 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+        <h1>Todo List ({this.props.incompleteCount})</h1>
         </header>
+
+        <label className="hide-completed">
+          <input
+            type="checkbox"
+            readOnly
+            checked={this.state.hideCompleted}
+            onClick={this.toggleHideCompleted.bind(this)}
+          />
+          Hide Completed Tasks
+        </label>
+
         <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
           <input
             type="text"
@@ -61,6 +92,7 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   }
 })(App);
